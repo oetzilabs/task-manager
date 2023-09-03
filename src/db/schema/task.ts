4b786039c6764b0879e84720047d8a9f7440e360
Özgür Isbert <oezguerisbert@gmail.com>
2023-09-03 15:12:35 +0200

    chores: code cleanup

--- src/db/schema/task.ts
+++ src/db/schema/task.ts
@@ -8,10 +8,6 @@ export type TaskStatus = (typeof task_status.enumValues)[number];
 export const task_priority = pgEnum("task_priority", ["low", "medium", "high"]);
 export type TaskPriority = (typeof task_priority.enumValues)[number];
 
-// Tasks should have a title, description, and a due date.
-// Tasks should have a status (e.g. "incomplete", "in progress", "complete").
-// Tasks should have a priority (e.g. "low", "medium", "high").
-// Tasks should be assigned to a users.
 export const tasks = pgTable("task", {
   id: uuid("id").notNull().primaryKey().defaultRandom(),
   title: text("title").notNull(),
